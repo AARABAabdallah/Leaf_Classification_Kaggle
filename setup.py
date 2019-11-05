a9acd194bc331375ad7a2eec97ffc3d972a47f03
AARABA <aaraba.abdallah@gmail.com>
2019-11-04 22:55:58 -0500

    Initial Commit!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Leaf_Classification_Kaggle',
+    author='AARABA_RAMI',
+    license='',
+)
