9f1b83c4a2b937f380e7783e0300b86a02e31a96
aaraba <aaraba10abdallah@gmail.com>
2019-11-17 18:15:13 -0500

    cleaning test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,4 @@
-import data.data_manipulation as dm
 import models.logistical_regression_model as lr
-from sklearn.metrics import log_loss
-import numpy as np
 
 def main():
     log_reg = lr.LogisticalRegressionModel()
