b49c99a8e29427d63bbc46f19dc652f48093ebec
aaraba <aaraba10abdallah@gmail.com>
2019-11-17 23:02:16 -0500

    Finishing logistical regression model

--- src/test.py
+++ src/test.py
@@ -2,9 +2,12 @@ import models.logistical_regression_model as lr
 
 def main():
     log_reg = lr.LogisticalRegressionModel()
-    log_reg.train_model()
-    training_loss = log_reg.calculate_loss()
-    print(training_loss)
+    log_reg.train_model(type='all_data')
+    #training_loss = log_reg.calculate_training_loss(type= 'data_splited')
+    #test_loss = log_reg.calculate_training_loss('data_splited')
+    #print(training_loss)
+    #print()
+    #print(test_loss)
 
 if __name__ == "__main__":
     main()
