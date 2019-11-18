fbf26f5010e420b4fa9f93882ecd948b64b1daab
aaraba <aaraba10abdallah@gmail.com>
2019-11-18 08:15:47 -0500

    adding ids lists to data_manipulation file

--- src/data/data_manipulation.py
+++ src/data/data_manipulation.py
@@ -13,16 +13,23 @@ class DataManipulation:
         self.labels_train = None
         self.labels_test = None
         self.data_unlabeled = None
+        self.ids_all_data_train = None
+        self.ids_splited_data_train = None
+        self.ids_splited_data_test = None
+        self.ids_data_test = None
 
 
     def load_data(self):
         raw_data = pd.read_csv("../data_sets/raw/train.csv")
-        raw_data = raw_data.drop("id", axis=1)
+        #raw_data = raw_data.drop("id", axis=1)
         ones = [1 for i in range(len(raw_data))] # Adding Bias !
         raw_data["ones"] = ones
         labels = raw_data["species"]
         labels = np.array(labels)
+        self.ids_all_data_train = raw_data["id"]
+        self.ids_all_data_train = np.array(self.ids_all_data_train)
         raw_data = raw_data.drop("species", axis=1)
+        raw_data = raw_data.drop("id", axis=1)
         raw_data = np.array(raw_data)
         self.data = raw_data
         self.labels = labels
@@ -30,6 +37,7 @@ class DataManipulation:
 
     def load_unlabeled_data(self):
         raw_data = pd.read_csv("../data_sets/raw/test.csv")
+        self.ids_data_test = raw_data["id"]
         raw_data = raw_data.drop("id", axis=1)
         ones = [1 for i in range(len(raw_data))]  # Adding Bias
         raw_data["ones"] = ones
@@ -39,6 +47,7 @@ class DataManipulation:
     def split_data(self):
         data_train,labels_train = [],[]
         data_test,labels_test = [],[]
+        self.ids_splited_data_train, self.ids_splited_data_test = [],[]
         labels_uniq = np.unique(self.labels)
         dict_labels = {k:0 for k in list(labels_uniq)}
 
@@ -46,13 +55,16 @@ class DataManipulation:
             if dict_labels[self.labels[i]]<8:
                 data_train.append(self.data[i])
                 labels_train.append(self.labels[i])
+                self.ids_splited_data_train.append(self.ids_all_data_train[i])
             else:
                 data_test.append(self.data[i])
                 labels_test.append(self.labels[i])
+                self.ids_splited_data_test.append(self.ids_all_data_train[i])
             dict_labels[self.labels[i]]+=1
 
         self.data_train,self.labels_train = np.array(data_train),np.array(labels_train)
         self.data_test,self.labels_test = np.array(data_test),np.array(labels_test)
+        self.ids_splited_data_train, self.ids_splited_data_test = np.array(self.ids_splited_data_train), np.array(self.ids_splited_data_test)
 
     def get_data(self):
         return self.data, self.labels
