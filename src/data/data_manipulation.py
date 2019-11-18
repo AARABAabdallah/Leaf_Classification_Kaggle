8feff9c107bb03453417218e422f5572b5b12004
aaraba <aaraba10abdallah@gmail.com>
2019-11-17 22:38:36 -0500

    add get methods in data_manipulaion

--- src/data/data_manipulation.py
+++ src/data/data_manipulation.py
@@ -6,9 +6,16 @@ class DataManipulation:
 
     def __init__(self):
         self.NUMBER_OF_SPECIES = 99
+        self.data = None
+        self.labels = None
+        self.data_train = None
+        self.data_test = None
+        self.labels_train = None
+        self.labels_test = None
+        self.data_unlabeled = None
 
-    def load_data_train(self):
 
+    def load_data(self):
         raw_data = pd.read_csv("../data_sets/raw/train.csv")
         raw_data = raw_data.drop("id", axis=1)
         ones = [1 for i in range(len(raw_data))] # Adding Bias !
@@ -19,31 +26,42 @@ class DataManipulation:
         raw_data = np.array(raw_data)
         self.data = raw_data
         self.labels = labels
-        return raw_data, labels
+        self.split_data()
 
-    def load_data_test(self):
+    def load_unlabeled_data(self):
         raw_data = pd.read_csv("../data_sets/raw/test.csv")
         raw_data = raw_data.drop("id", axis=1)
         ones = [1 for i in range(len(raw_data))]  # Adding Bias
         raw_data["ones"] = ones
         raw_data = np.array(raw_data)
         self.data_unlabeled = raw_data
-        return raw_data
 
     def split_data(self):
-        data_train,label_train = [],[]
-        data_test,label_test = [],[]
+        data_train,labels_train = [],[]
+        data_test,labels_test = [],[]
         labels_uniq = np.unique(self.labels)
         dict_labels = {k:0 for k in list(labels_uniq)}
 
         for i in range(len(self.data)):
             if dict_labels[self.labels[i]]<8:
                 data_train.append(self.data[i])
-                label_train.append(self.labels[i])
+                labels_train.append(self.labels[i])
             else:
                 data_test.append(self.data[i])
-                label_test.append(self.labels[i])
+                labels_test.append(self.labels[i])
             dict_labels[self.labels[i]]+=1
 
-        self.data_train,self.label_train = np.array(data_train),np.array(label_train)
-        self.data_test,self.label_test = np.array(data_test),np.array(label_test)
+        self.data_train,self.labels_train = np.array(data_train),np.array(labels_train)
+        self.data_test,self.labels_test = np.array(data_test),np.array(labels_test)
+
+    def get_data(self):
+        return self.data, self.labels
+
+    def get_training_data(self):
+        return self.data_train, self.labels_train
+
+    def get_test_data(self):
+        return self.data_test, self.labels_test
+
+    def get_unlabeled_data(self):
+        return self.data_unlabeled
