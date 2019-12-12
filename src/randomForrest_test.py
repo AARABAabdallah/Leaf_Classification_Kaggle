ad7046577229c51bb183ce6383dd375de1c2c0a6
aaraba <aaraba10abdallah@gmail.com>
2019-12-12 17:59:48 -0500

    WIP

--- src/randomForrest_test.py
+++ src/randomForrest_test.py
@@ -5,7 +5,7 @@ def main():
     #rand_forrest.train_model_cross_validation()
     #rand_forrest.train_model_features()
     rand_forrest.load_model_features()  #############
-    #rand_forrest.submit_test_results_features()
+    rand_forrest.submit_test_results_features()
     training_all_data_loss = rand_forrest.calculate_training_loss()  #############
     print("training_loss_model_features: ",training_all_data_loss)
 
@@ -26,7 +26,7 @@ def main():
     #rand_forrest.train_model_pca()
     #rand_forrest.train_model_pca_cross_validation()
     rand_forrest.load_model_pca()
-    #rand_forrest.submit_test_results_pca()
+    rand_forrest.submit_test_results_pca()
     training_pca_loss = rand_forrest.calculate_training_loss_pca_data()
     print("training_loss_model_pca: ", training_pca_loss)
 
