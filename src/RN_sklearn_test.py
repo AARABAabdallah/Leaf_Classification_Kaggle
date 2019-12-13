f5a3c6844ce6ec28663479392fab1f764a4d7308
Rami <abdellah.rami98@gmail.com>
2019-12-12 22:10:04 -0500

    Changes in the notebook

--- src/RN_sklearn_test.py
+++ src/RN_sklearn_test.py
@@ -5,12 +5,11 @@ def main():
 
     rn_skl = RN_skl.RN_sklearn_model()
 
-    rn_skl.load_model_features_images()
+    rn_skl.load()
     print(rn_skl.get_training_loss_features_images())
     # rn_skl.features_cross_valid(layer2_range=[80,100,140,160],layer3_range=range(0),layer1_range=range(10,241,5))
     # print(rn_skl.get_training_loss())
 
-    rn_skl.submit_test_results_images_features()
-
+    rn_skl.submit_test_results_features()
 if __name__ == "__main__":
     main()
